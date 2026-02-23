87d91102ac19f4db0ed5943ec5337d42ed8e9569
Dmytro Farbun <dimofarb@gmail.com>
2026-02-23 11:13:51 +0200

    feat: add markup for Join component

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -1,7 +1,14 @@
 import css from "./Join.module.css";
+import Link from "next/link";
 
 export default function Join() {
   return (
-    <h1>Join</h1>
+    <section className="section">
+      <div className="container">
+        <h2 className={css.sectionHeading}>Приєднуйтесь до нашої спільноти</h2>
+        <p className={css.paragraph}>Долучайтеся до мандрівників, які діляться своїми історіями та надихають на нові пригоди.</p>
+        <Link className={css.registerBtn} href="/register">Зареєструватися</Link>
+      </div>
+    </section>
   );
 }
