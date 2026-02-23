76bae9bd8fd89b34fc0abc24fb6dd3c0cffc3409
Dmytro Farbun <dimofarb@gmail.com>
2026-02-23 11:16:39 +0200

    refactor: changed className for container

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Join() {
   return (
     <section className="section">
-      <div className="container">
+      <div className={`container ${css.container}`}>
         <h2 className={css.sectionHeading}>Приєднуйтесь до нашої спільноти</h2>
         <p className={css.paragraph}>Долучайтеся до мандрівників, які діляться своїми історіями та надихають на нові пригоди.</p>
         <Link className={css.registerBtn} href="/register">Зареєструватися</Link>
