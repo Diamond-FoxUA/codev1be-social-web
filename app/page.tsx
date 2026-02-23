85967dea58d185e773d374a3023b67863424410e
iryna-pona <ipona3710@gmail.com>
2026-02-23 23:40:40 +0200

    conflict resolution in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 import './globals.css';
 import Hero from '@/components/Hero/Hero';
->>>>>>> origin/main
 import About from '@/components/About/About';
 import OurTravellers from "@/components/OurTravellers/OurTravellers";
 import Join from '@/components/Join/Join';
