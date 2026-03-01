c4d9babfa710715a6f5706c431d54e30aef35f1f
bndnt <67003594+bndnt@users.noreply.github.com>
2026-03-01 17:23:29 -0500

    select debug with draft

--- middleware.ts
+++ middleware.ts
@@ -10,11 +10,11 @@ export function middleware(req: NextRequest) {
   const isAuthenticated = Boolean(accessToken || refreshToken);
 
   // PRIVATE ROUTES
-  const isPrivateRoute =
-    pathname.startsWith('/profile') ||
-    pathname === '/stories/create' ||
-    pathname === '/edit' ||
-    (pathname.startsWith('/stories/') && pathname.endsWith('/edit'));
+  const isPrivateRoute = pathname.startsWith('/profile');
+  //  ||
+  // pathname === '/stories/create' ||
+  // pathname === '/edit' ||
+  // (pathname.startsWith('/stories/') && pathname.endsWith('/edit'));
 
   // GUEST ONLY ROUTES
   const isGuestRoute = pathname === '/login' || pathname === '/register';
@@ -35,7 +35,7 @@ export function middleware(req: NextRequest) {
 export const config = {
   matcher: [
     '/profile',
-    // '/stories/create',
+    '/stories/create',
     '/stories/:storyId/edit',
     '/edit',
     '/login',
