c752068560ed6d1f513243c860974ed333aacec4
Dmytro Farbun <dimofarb@gmail.com>
2026-03-03 01:40:19 +0200

    styles: change notFound page logo icon to be without text

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import css from './not-found.module.css';
 import LinkButton from '@/components/LinkButton/LinkButton';
-import Logo from '@/components/Logo/Logo';
+import Image from 'next/image';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -19,7 +19,14 @@ export default function NotFound() {
     <div className={`container ${css.container}`}>
       <div className={css.messageContainer}>
         <div className={css.StatusCode}>
-          <Logo />
+          <Image
+            src="/svg/logo.svg"
+            alt="Лого 'Подорожники'"
+            width={60}
+            height={60}
+            priority
+            className={css.logoIcon}
+          />
           <h1 className={css.messageError}>404</h1>
         </div>
         <h2 className={css.messageTitle}>Сторінку не знайдено</h2>
