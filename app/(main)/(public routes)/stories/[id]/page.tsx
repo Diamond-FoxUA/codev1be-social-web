5a59c9e67efdac5f196e1df842e51c3f83414645
Dmytro Farbun <dimofarb@gmail.com>
2026-03-04 10:18:49 +0200

    refactor(StoryPage): update imports after folder structure change

---
+++ app/(main)/(public routes)/stories/[id]/page.tsx
@@ -0,0 +1,26 @@
+// app/stories/[id]/page.tsx
+import StoryPage from '@/components/StoryPage/StoryPage';
+import { getSingleStory } from '@/lib/api/story';
+import css from '@/components/StoryPage/StoryPage.module.css';
+import PopularStoriesSection from '@/components/PopularStoriesSection/PopularStoriesSection';
+
+type Props = {
+  params: { id: string };
+};
+
+export default async function Page({ params }: Props) {
+  const { id } = await params;
+  const story = await getSingleStory(id);
+  console.log(story);
+
+  return (
+    <>
+      <div className={css.wrapper}>
+        <StoryPage story={story} />
+      </div>
+      <div className={css.popularWrapper}>
+        <PopularStoriesSection />
+      </div>
+    </>
+  );
+}
